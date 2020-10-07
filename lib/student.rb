97af78e75e33fc33f35867b06e366c841785b646
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-07 17:31:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,8 +53,7 @@ class Student
   
   
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
+    student = self.new()row[0]
     student.name = row[1]
     student.grade = row[2]
     student
